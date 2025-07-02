ca8fd3046ac11a2b4f24e835010044a4a8631f4a
anshu-raj-6041 <anshu604125@gmail.com>
2025-07-02 13:11:54 +0530

    File

---
+++ 8-Module/package/_init_.py
@@ -0,0 +1,1 @@
+# _init_.py is a special file used in Python to define packages and initialize their namespaces
---
+++ 8-Module/test.py
@@ -0,0 +1,7 @@
+from package.maths import *
+from package.subpackages.mult import multiply
+
+
+print(addition(2, 5))
+print(subtraction(2, 5))
+print(multiply(2, 5))
